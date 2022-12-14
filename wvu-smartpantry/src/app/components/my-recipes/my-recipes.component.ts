beb6a05ddbf6399a035f50aa1c9871473846e687
fsa0009 <84762334+fsa0009@users.noreply.github.com>
2022-12-14 12:34:38 -0500

    fixed recipes and home page

--- wvu-smartpantry/src/app/components/my-recipes/my-recipes.component.ts
+++ wvu-smartpantry/src/app/components/my-recipes/my-recipes.component.ts
@@ -20,6 +20,8 @@ import { map, Observable } from "rxjs";
 
 export class MyRecipesComponent implements OnInit  {
 
+
+  
     printingUID = DashboardComponent.something; 
     branchURL = '/recipes/global/' ;
     recipeNum = 1; 
@@ -46,9 +48,42 @@ export class MyRecipesComponent implements OnInit  {
     
     console.log(this.db.list('recipes'));
 
-      this.getAll().subscribe(p => this.recipe = p)
+      this.getAll().subscribe(p => this.recipe = p);
+
+      this.allRecipes = this.getAll().subscribe(p => this.recipe = p);
+
+      console.log(this.allRecipes[1][1] + "lasty");
+
+
+
+      
 
   console.log(DashboardComponent.something);
 
 }
 }
+
+
+/*
+
+    <div class="cards border border-dark border-2 me-4 mb-5" *ngFor="let data of recipe ; " style="width: 24rem;">
+      <img src="{{data.G_ImagePath}}" class="card-img-top" alt="..." style="height:350px ; width:378px; ">
+      <div class="card-body justify-content-center">
+        <h3 class="card-num text-center fw-bold">{{data.A_RecipeName}}</h3>
+        <p class="card-title text-center"> Calories: {{data.D_Calories}} Cal</p>
+        <p class="card-title text-center"> Coocking Time: {{data.C_CookingTime}} mins</p>
+        <p class="card-title text-center"> Servings: {{data.E_Servings}} servings</p>
+
+        <a href="#" class="visually-hidden-focusable stretched-link text-center">mk</a>
+
+
+        <div class="d-grid gap-2">
+          <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#firstR">
+            View Recipe
+          </button>
+
+        </div>
+      </div>
+    </div>
+
+*/
